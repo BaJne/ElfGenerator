b498657dc634e5201c2dec37104c6b66e49c6067
Branislav Jevtic <branislavgrom@gmail.com>
2022-03-05 21:01:48 +0100

    Make Section abstract
    -- Making new plan for section class api

--- src/elf/section/Section.java
+++ src/elf/section/Section.java
@@ -6,5 +6,21 @@ package elf.section;
  * An object file may have inactive space. The various headers and the sections might not ‘‘cover’’
  * every byte in an object file. The contents of the inactive data are unspecified.
  */
-public class Section {
+public abstract class Section {
+    // TODO:
+    // 1. Make API:
+    //      * Fill compiled code
+    //      * Make relocation data and insert inside rel section.
+    //      * Find way to update multiple dependent sections - probably from most top level.
+    // 2. Every Section has it's entry inside section header table. (need to link this properly, and to fast change
+    //    corresponding entries
+    // 3. -
+
+    // For debug purpose
+    private String name;
+
+    public Section(String name) {
+        this.name = name;
+    }
+
 }
